798ec944bf6550e47f83e08a9e53fa811c72c6bd
Matt Brannen <brannen.matthew@gmail.com>
2023-11-16 23:36:20 -0700

    Added art for Stim Added shader for special quality added another item

---
+++ Code/Buffs/Health/StimPack/StimPack.cs
@@ -0,0 +1,12 @@
+using Mozzie.Code.Player;
+
+namespace Mozzie.Code.Buffs.Health.StimPack;
+
+public class StimPack : AdditiveBuffBase
+{
+    public override BuffAffectType BuffAffectType => BuffAffectType.Player;
+    public override string Name => "Stim Pack";
+    public override StatType StatType => StatType.Health;
+    public override Rarity Rarity => Rarity.Uncommon;
+    public override int Value => 10;
+}
